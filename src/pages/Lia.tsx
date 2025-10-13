f751828751630948624998948fcc2300069800f5
Kayke Nícolas <kaykenicolas007@gmail.com>
2025-10-13 15:19:48 -0300

    feat: adiciona primeira imagem de Lia

--- src/pages/Lia.tsx
+++ src/pages/Lia.tsx
@@ -1,11 +1,18 @@
 import Aqua from "../assets/aqua.png";
+import Lia1 from "../assets/Lia1.jpg";
+import "./Lia.css";
 
 function Lia() {
   return (
-    <div>
-      <h1>Lia</h1>
-      <img src={Aqua} alt="Aqua" />
-    </div>
+    <>
+      <div>
+        <h1>Lia</h1>
+        <img src={Aqua} alt="Aqua" />
+      </div>
+      <div className="text">
+        <p>Aqua is a character from the game.</p>
+      </div>
+    </>
   );
 }
 
